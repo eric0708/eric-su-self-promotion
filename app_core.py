749d9f78d85409883ecd0f543a663294abcbab5e
eric0708 <b05901188@ntu.edu.tw>
2021-05-21 17:12:15 +0800

    intro added

--- app_core.py
+++ app_core.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 import os
 import configparser
+from pathlib import Path
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -34,18 +35,21 @@ def callback():
 # 學你說話
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
-
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef" and event.message.text == "誰是世界上最漂亮的媽媽":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="小德蕙")
-        )
     
     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
+
+        if event.message.text == "簡介":
+            txt = Path('replies/intro.txt').read_text()
+            txt = txt.replace('\n', '')
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=txt)
+            )
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=event.message.text)
+            )
 
 if __name__ == "__main__":
     app.run()
