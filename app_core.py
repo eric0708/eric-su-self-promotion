e14c6ba4465456d6bc9e0efbaa6118f00754a57b
eric0708 <b05901188@ntu.edu.tw>
2021-05-23 22:27:14 +0800

    done

--- app_core.py
+++ app_core.py
@@ -23,60 +23,9 @@ handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 # set LINE Chatbot rich menu
 
-"""
-headers = {"Authorization":"Bearer "+config.get('line-bot', 'channel_access_token'),"Content-Type":"application/json"}
-
-body = {
-    "size": {"width": 2500, "height": 1686},
-    "selected": "true",
-    "name": "Controller",
-    "chatBarText": "Controller",
-    "areas":[
-        {
-          "bounds": {"x": 0, "y": 0, "width": 625, "height": 843},
-          "action": {"type": "message", "text": "self introduction"}
-        },
-        {
-          "bounds": {"x": 625, "y": 0, "width": 625, "height": 843},
-          "action": {"type": "message", "text": "education"}
-        },
-        {
-          "bounds": {"x": 1250, "y": 0, "width": 625, "height": 843},
-          "action": {"type": "message", "text": "research interests"}
-        },
-        {
-          "bounds": {"x": 1875, "y": 0, "width": 625, "height": 843},
-          "action": {"type": "message", "text": "research experiences"}
-        },
-        {
-          "bounds": {"x": 0, "y": 843, "width": 625, "height": 843},
-          "action": {"type": "message", "text": "projects"}
-        },
-        {
-          "bounds": {"x": 625, "y": 843, "width": 625, "height": 843},
-          "action": {"type": "message", "text": "skills"}
-        },
-        {
-          "bounds": {"x": 1250, "y": 843, "width": 625, "height": 843},
-          "action": {"type": "message", "text": "certifications"}
-        },
-        {
-          "bounds": {"x": 1875, "y": 843, "width": 625, "height": 843},
-          "action": {"type": "message", "text": "extracurricular activities"}
-        }
-    ]
-  }
-
-req = requests.request('POST', 'https://api.line.me/v2/bot/richmenu', 
-                       headers=headers,data=json.dumps(body).encode('utf-8'))
-print(req.text)
-"""
 richmenu_id = "richmenu-bca18a3ad10c9ffc246895d3dfbaf564"
 headers = {"Authorization":"Bearer "+config.get('line-bot', 'channel_access_token'),"Content-Type":"application/json"}
-"""
-f = open("images/richmenu.jpg", 'rb')
-line_bot_api.set_rich_menu_image(richmenu_id, "image/jpeg", f.read())
-"""
+
 req = requests.request('POST', 'https://api.line.me/v2/bot/user/all/richmenu/'+richmenu_id, 
                        headers=headers)
 #print(req.text)
