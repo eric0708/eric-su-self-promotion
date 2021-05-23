6f34f7fee57aadedbb642cbd1a233b27889c9df0
eric0708 <b05901188@ntu.edu.tw>
2021-05-23 16:17:38 +0800

    add todo function

--- app_core.py
+++ app_core.py
@@ -206,7 +206,7 @@ def delete_all_todos(username):
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
 
-    postgres_delete_query = f"""Delete from todo_list where name = %s"""
+    postgres_delete_query = f"""Delete * from todo_list where name = %s"""
     
     cursor.executemany(postgres_delete_query, username)
     conn.commit()
